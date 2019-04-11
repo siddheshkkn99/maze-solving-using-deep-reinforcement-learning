643137af51bf56d3aa2860ff8b7390dba629c721
ani5ruth <45996427+ani5ruth@users.noreply.github.com>
2019-04-11 19:09:58 +0530

    Update test.py

--- MazeSolver/test.py
+++ MazeSolver/test.py
@@ -1,33 +1,34 @@
 # ---------------program to solve a maze  using deep reinforced learning technique---------------------------------
 
 # ---------------------------------------------------------------------------------
-# importing maze solver program
-# importing libraries and functions
-from __future__ import print_function
-from IPython import get_ipython
-import os, sys, time, datetime, json, random
-import numpy as np
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.optimizers import SGD , Adam, RMSprop
-from keras.layers.advanced_activations import PReLU
-import matplotlib.pyplot as plt
+# importing maze generator and solver programs
 from mazeSolve import *
 from mazeGenerator import *
 # ---------------------------------------------------------------------------------
-# developing a sample maze
-
-# maze structure stored in a 2d numpy array with floating values from 0.0-1.0
-# which will be plotted on tkinter 1.0 indicates free cell and 0.0 represents blocked cell
-# the floating numbers also denote the color of the cell 1:white 0:black
-# the rat is starting at cell (0,0) (top-left cell) and the cheese is placed at cell (9,9)
-
-# generating sample 5X5 maze
-maze=MazeGen(5,5).getMaze()
+# developing a sample maze in the form of an array with values 0(blocked cell) or 1(free cell)
+print("Enter no. of rows and columns in maze")
+nrows,ncols = map(int,input().split())
+maze=MazeGen(nrows,ncols).getMaze()
 # ----------------------------------------------------------------------------------
-
+'''
+# sample maze
+maze = np.array([
+    [ 1.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.],
+    [ 1.,  1.,  1.,  1.,  1.,  0.,  1.,  1.,  1.,  1.],
+    [ 1.,  1.,  1.,  1.,  1.,  0.,  1.,  1.,  1.,  1.],
+    [ 0.,  0.,  1.,  0.,  0.,  1.,  0.,  1.,  1.,  1.],
+    [ 1.,  1.,  0.,  1.,  0.,  1.,  0.,  0.,  0.,  1.],
+    [ 1.,  1.,  0.,  1.,  0.,  1.,  1.,  1.,  1.,  1.],
+    [ 1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.],
+    [ 1.,  1.,  1.,  1.,  1.,  1.,  0.,  0.,  0.,  0.],
+    [ 1.,  0.,  0.,  0.,  0.,  0.,  1.,  1.,  1.,  1.],
+    [ 1.,  1.,  1.,  1.,  1.,  1.,  1.,  0.,  1.,  1.]
+])
+'''
 # feeding sample maze to the program
 qmaze = Qmaze(maze)
+
+# displaying input maze
 show(qmaze)
 plt.show()
 
@@ -37,5 +38,6 @@ qtrain(model, maze, epochs=1000, max_memory=8*maze.size, data_size=32)
 
 # playing a game on the built model
 play_game(model,qmaze,(0,0))
+# displaying solved maze
 show(qmaze)
 plt.show()
